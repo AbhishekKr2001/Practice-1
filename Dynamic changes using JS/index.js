6b929df916cab9473e43064695929b3a82374bdb
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2024-05-02 09:02:12 +0530

    Add files via upload

---
+++ Dynamic changes using JS/index.js
@@ -0,0 +1,48 @@
+console.log("hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const p1 = document.getElementById("id1");
+// const p2 = document.getElementById("id2");
+// var div1 = document.getElementById("div");
+// function changePara() {
+//     p1.innerText = "THIS HAS BEEN CHANGED BY CLICKING ON BUTTON 1";
+//     p2.innerText = "THIS HAS BEEN CHANGED BY CLICKING ON BUTTON 1";
+// }
+// function changeStyle() {
+//     const all = document.getElementsByTagName("p");
+//     for (var i = 0; i < all.length; i++) {
+
+//         all[i].style.color = "red";
+//     }
+
+// }
+// function createElementp() {
+//     alert("Button 3 has been clicked");
+//     let p = document.createElement('P');
+//     p.innerText = "This has been added by clicking on button 3";
+//     div1.prepend(p);
+
+
+// }
+
+// document.getElementById("button1").addEventListener('click', changePara);
+
+// document.getElementById("button2").addEventListener('click', changeStyle);
+// document.getElementById("button3").addEventListener('click', createElementp);
